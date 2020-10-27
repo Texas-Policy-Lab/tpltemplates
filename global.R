41f63bdde6f045965df67d3c72514906434b8a1f
Joli Holmes <holmesjoli@gmail.com>
2020-10-27 14:02:34 -0500

    updated config name

--- global.R
+++ global.R
@@ -6,7 +6,7 @@ options(encoding = "UTF-8")
 
 sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
 
-config <- yaml::read_yaml("./mainDashboard.yaml")
+config <- yaml::read_yaml("./dashboard.yaml")
 
 ## GLOBALS
 TPL_URL <- config$tpl_url
