7d1e6211f1eb68f48d9f94c08f650cff927ce7a8
Joli Holmes <holmesjoli@gmail.com>
2020-10-30 09:04:31 -0500

    added create report function and moved the main function of create_app into copy_template

--- R/templates.R
+++ R/templates.R
@@ -1,8 +1,10 @@
-#' @title Create dashboard
-#' @description Creates an application set-up that includes TPL styles in the css folder
-#' @param pth string. The path root directory of the application.
+#' @title Copy template
+#' @description Copies template files from www and the specified template directory
+#' @param template_dir string. The name of the template dir to copy files from.
 #' @export
-create_dashboard <- function(pth) {
+copy_template <- function(pth,
+                          template_dir,
+                          ignore = "R/templates.R") {
 
   dirs <- c("R",
             "www",
@@ -10,25 +12,40 @@ create_dashboard <- function(pth) {
             file.path("www", "js"),
             file.path("www", "images"),
             file.path("www", "svg")
-            )
-  
-  fls <- c("dashboard.yaml", "global.R", "server.R", "ui.R")
-  
-  lapply(dirs, function(dir, pth) dir.create(file.path(pth, dir)), pth = pth)
-  
-  lapply(c(fls, list.files(dirs, full.names = T)),
-         function(fl, pth) {
-           file.copy(from = fl, to = file.path(pth, fl))}, pth = pth)
+  )
 
+  lapply(dirs, function(dir, pth) dir.create(file.path(pth, dir)), pth = pth)
+
+  fls <- c(list.files(template_dir, full.names = TRUE, recursive = TRUE),
+           list.files(dirs, full.names = TRUE, recursive = TRUE))
+
+  fls <- fls[grepl(ignore, fls) == FALSE]
+
+  lapply(fls, function(fl, pth) {
+
+           file.copy(from = fl,
+                     to = file.path(pth, gsub(template_dir, ".", fl)))
+    }, pth = pth)
+}
+
+#' @title Create dashboard
+#' @description Creates an application set-up that includes TPL styles in the css folder
+#' @param pth string. The path root directory of the application.
+#' @export
+create_dashboard <- function(pth,
+                             template_dir = "app_template") {
+
+  copy_template(pth = pth,
+                template_dir = template_dir)
 }
 
 #' @title Create report
 #' @description Creates a rmarkdown report set-up that includes TPL styles in the css folder
 #' @param pth string. The path root directory of the application.
 #' @export
-create_report <- function() {
-  
-  
-  
-  
+create_report <- function(pth,
+                          template_dir = "report_template") {
+
+  copy_template(pth = pth,
+                template_dir = template_dir)
 }
