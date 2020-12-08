9df234056b09b26558ac421a7323329b695ab259
Joli Holmes <holmesjoli@gmail.com>
2020-12-08 16:18:25 -0600

    refactored template code to make it work for report

--- R/templates.R
+++ R/templates.R
@@ -1,31 +1,12 @@
-#' @title Copy template
-#' @description Copies template files from www and the specified template directory
-#' @param template_dir string. The name of the template dir to copy files from.
-#' @export
-copy_template <- function(pth,
-                          template_dir,
-                          ignore = "R/templates.R") {
-
-  dirs <- c("R",
-            "www",
-            file.path("www", "css"),
-            file.path("www", "js"),
-            file.path("www", "images"),
-            file.path("www", "svg")
-  )
-
-  lapply(dirs, function(dir, pth) dir.create(file.path(pth, dir)), pth = pth)
-
+list_files <- function(template_dir,
+                       dirs, 
+                       ignore = "R/templates.R") {
+  
   fls <- c(list.files(template_dir, full.names = TRUE, recursive = TRUE),
            list.files(dirs, full.names = TRUE, recursive = TRUE))
-
+  
   fls <- fls[grepl(ignore, fls) == FALSE]
-
-  lapply(fls, function(fl, pth) {
-    
-           file.copy(from = fl,
-                     to = file.path(pth, gsub(template_dir, ".", fl)))
-    }, pth = pth)
+  
 }
 
 #' @title Create dashboard
@@ -33,10 +14,21 @@ copy_template <- function(pth,
 #' @param pth string. The path root directory of the application.
 #' @export
 create_dashboard <- function(pth,
-                             template_dir = "app_template") {
+                             template_dir = "app_template",
+                             dirs = list.dirs(c("R", "www"))) {
 
-  copy_template(pth = pth,
-                template_dir = template_dir)
+  lapply(dirs, function(dir, pth) dir.create(file.path(pth, dir)), pth = pth)
+  
+  fls <- list_files(template_dir = template_dir,
+                    dirs = dirs)
+
+  lapply(fls, function(fl, pth) {
+    
+    file.copy(from = fl,
+              to = file.path(pth, 
+                             gsub(template_dir, ".", fl))
+    )
+  }, pth = pth)
 }
 
 #' @title Create report
@@ -44,8 +36,21 @@ create_dashboard <- function(pth,
 #' @param pth string. The path root directory of the application.
 #' @export
 create_report <- function(pth,
-                          template_dir = "report_template") {
+                          template_dir = "report_template",
+                          dirs = list.dirs(c("R", "www"))) {
 
-  copy_template(pth = pth,
-                template_dir = template_dir)
+  dirs_special <- c("R", "css", "js", "images", "svg")
+
+  lapply(dirs_special, function(dir, pth) dir.create(file.path(pth, dir)), pth = pth)
+
+  fls <- list_files(template_dir = template_dir,
+                    dirs = dirs)
+
+  lapply(fls, function(fl, pth) {
+    
+    file.copy(from = fl,
+              to = file.path(pth, 
+                             gsub(paste(template_dir, "www", sep = "|"), ".", fl))
+    )
+  }, pth = pth)
 }
